0d3c9b483cb5b26ca310e5751b8c2220311108e3
Federico Salafia <federico.salafia@miracledevs.com>
2023-03-15 16:01:15 -0300

    changed sh test file

--- products-api-get.sh
+++ products-api-get.sh
@@ -1,3 +1,6 @@
 curl --location 'https://localhost:7275/product/cached' & 
 curl --location 'https://localhost:7275/product/cached' &
-curl --location 'https://localhost:7275/product/cached'
+curl --location 'https://localhost:7275/product/cached' & 
+curl --location 'https://sheltered-spire-31397.herokuapp.com/product/cached' & 
+curl --location 'https://sheltered-spire-31397.herokuapp.com/product/cached' &
+curl --location 'https://sheltered-spire-31397.herokuapp.com/product/cached'
